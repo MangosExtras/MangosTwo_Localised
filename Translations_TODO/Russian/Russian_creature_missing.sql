a541391b5d50069c98ba9004587685b6177fddaf
gromchek <gromchek@protonmail.com>
2020-09-26 13:38:45 +0100

    [Russian] Added some new translations

--- Translations_TODO/Russian/Russian_creature_missing.sql
+++ Translations_TODO/Russian/Russian_creature_missing.sql
@@ -2,12 +2,5 @@
 SET NAMES 'utf8';
 UPDATE `locales_creature` SET `name_loc8`='Waypoint (Only GM can see it)',`subname_loc8`='Visual' WHERE `entry`=1;
 UPDATE `locales_creature` SET `subname_loc8`='The Head of The Black Flight' WHERE `entry`=11583;
-UPDATE `locales_creature` SET `subname_loc8`='Silverwing Supply Officer (Neutral)' WHERE `entry`=20010;
-UPDATE `locales_creature` SET `subname_loc8`='Silverwing Supply Officer (Friendly)' WHERE `entry`=20011;
-UPDATE `locales_creature` SET `subname_loc8`='Silverwing Supply Officer (Honored)' WHERE `entry`=20012;
-UPDATE `locales_creature` SET `subname_loc8`='Silverwing Supply Officer (Revered)' WHERE `entry`=20013;
-UPDATE `locales_creature` SET `subname_loc8`='Silverwing Supply Officer (Exalted)' WHERE `entry`=20014;
-UPDATE `locales_creature` SET `subname_loc8`='Warsong Supply Officer (Honored)' WHERE `entry`=20022;
-UPDATE `locales_creature` SET `subname_loc8`='Don Carlos\' Pet' WHERE `entry`=28168;
 
 SET NAMES 'latin1';
